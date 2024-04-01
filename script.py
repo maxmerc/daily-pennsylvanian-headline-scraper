cc406d7335691c30c66520004d8f9361bc8bfd64
Max Mercado <115499256+maxmerc@users.noreply.github.com>
2024-04-01 12:21:57 -0400

    test: script.py for most read div

--- script.py
+++ script.py
@@ -26,7 +26,7 @@ def scrape_data_point():
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        target_element = soup.find("div", class_="row section-etc hidden-sm hidden-xs").find("img", class_="img img-responsive")
+        target_element = soup.find("div", class_="row section-etc hidden-sm hidden-xs").find("div", class_="col-md-8")
         data_point = "" if target_element is None else target_element.text
         loguru.logger.info(f"Data point: {data_point}")
         return data_point
